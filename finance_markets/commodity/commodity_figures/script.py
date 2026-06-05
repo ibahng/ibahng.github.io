7eaa1762058bf69080a1ae5d8a12074f896494b4
ibahng <ibahng21@gmail.com>
2026-06-05 12:37:49 +0900

    commodity work

--- finance_markets/commodity/commodity_figures/script.py
+++ finance_markets/commodity/commodity_figures/script.py
@@ -3,15 +3,34 @@ HOME = os.getenv('HOME', '/Users/ingyubahng')
 sys.path.append(f"{HOME}/Workspaces/pyutils")
 import markets # type: ignore
 
-test = markets.yfdata(
-        ticker = 'CL=F',
-        start = '2020-01-01',
-        end = '2020-12-01',
-        interval = '1d',
+FRED_API_KEY = os.getenv('FRED_API_KEY', 'error')
+if FRED_API_KEY == "error":
+    print('FRED api key error')
+
+# test = markets.yfdata(
+#         ticker = 'CL=F',
+#         start = '2019-09-01',
+#         end = '2020-09-01',
+#         interval = '1d',
+#         )
+
+fred = markets.freddata(
+        code = 'DFF',
+        start = '1975-01-01',
+        end = '1985-01-01',
         )
 
-print(type(test.table))
-test.plot(
-        count = '2',
-        labels = 'm'
+fred.plot(
+        count = '1',
+        labels = 'y',
+        ylabel = 'Rate'
         )
+
+
+print("hello world")
+
+# print(type(test.table))
+# test.plot(
+#         count = '4',
+#         labels = 'm'
+#         )
