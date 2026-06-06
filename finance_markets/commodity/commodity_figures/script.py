748eab0505b578cb04b8bb631cc3e4870cca065b
ibahng <ibahng21@gmail.com>
2026-06-06 22:44:02 +0900

    finished commodities and updated webiste card subtopics

--- finance_markets/commodity/commodity_figures/script.py
+++ finance_markets/commodity/commodity_figures/script.py
@@ -7,30 +7,31 @@ FRED_API_KEY = os.getenv('FRED_API_KEY', 'error')
 if FRED_API_KEY == "error":
     print('FRED api key error')
 
-# test = markets.yfdata(
-#         ticker = 'CL=F',
-#         start = '2019-09-01',
-#         end = '2020-09-01',
-#         interval = '1d',
+test = markets.yfdata(
+        ticker = 'CL=F',
+        start = '2024-01-01',
+        end = '2026-06-03',
+        interval = '1d',
+        )
+
+test.plot(
+        count = '8',
+        labels = 'y'
+        )
+
+# fred = markets.freddata(
+#         code = 'DFF',
+#         start = '1975-01-01',
+#         end = '1985-01-01',
 #         )
 
-fred = markets.freddata(
-        code = 'DFF',
-        start = '1975-01-01',
-        end = '1985-01-01',
-        )
-
-fred.plot(
-        count = '1',
-        labels = 'y',
-        ylabel = 'Rate'
-        )
+# fred.plot(
+#         count = '1',
+#         labels = 'y',
+#         ylabel = 'Rate'
+#         )
 
 
-print("hello world")
+print("done")
 
 # print(type(test.table))
-# test.plot(
-#         count = '4',
-#         labels = 'm'
-#         )
